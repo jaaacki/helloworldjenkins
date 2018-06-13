6335ba2dede0f2a1c499a3ab540693dde97b8fb2
Jaaacki <jacki.da@gmail.com>
2018-06-13 23:44:07 +0800

    made changes to docker compose

--- src/index.php
+++ src/index.php
@@ -2,3 +2,10 @@
 $welcome = "hello world 34";
 echo $welcome;
 ?>
+
+cd /
+cd /helloworldjenkins
+docker stop helloworld
+docker rm helloworld
+docker build -t hello-world /helloworldjenkins
+docker run -d --name helloworld -5000:80 -v /home/te
